0a3f47b10343dcfdaf57ffd95ad385190d9d5b4a
javimammana <javiermammana.dev@gmail.com>
2025-07-23 11:27:59 -0300

    babyKaren

--- README.md
+++
@@ -1,2 +0,0 @@
-# camigabi
-# babygio
---
+++ js/datos.js
@@ -0,0 +1,7 @@
+const fecha = new Date("08/10/2025 14:00");
+const cumple = new Date("08/10/2025");
+const horaEvento = "14:00 a 18:00Hs";
+const direccionLink = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d163.43916626087736!2d-64.29040400067616!3d-31.122375221634154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2sar!4v1751856047173!5m2!1ses!2sar";
+const coordenadas = "-31.122410, -64.290415";
+const formAsistencia = "https://wa.me/5493513283382";
+const formCanciones = "https://forms.gle/D9trxxRy8nJUSyTZ8";
