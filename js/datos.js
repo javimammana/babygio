827e522b232cd562c21e3c5a255d405337d14881
javimammana <javiermammana.dev@gmail.com>
2025-07-25 11:27:27 -0300

    correccion mapa

--- js/datos.js
+++ js/datos.js
@@ -1,7 +1,7 @@
 const fecha = new Date("08/10/2025 14:00");
 const cumple = new Date("08/10/2025");
 const horaEvento = "17:00 a 20:00Hs";
-const direccionLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3406.9989427983537!2d-64.22722392507502!3d-31.359008393478664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432993f32def7e5%3A0x34e81025609b113a!2sRoberto%20Cayol%203830%2C%20X5008%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1753281670746!5m2!1ses-419!2sar";
-const coordenadas = "Roberto cayol 3830";
+const direccionLink = "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d851.7502569884917!2d-64.22407673042716!3d-31.35895085319143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDIxJzMyLjIiUyA2NMKwMTMnMjQuNCJX!5e0!3m2!1ses-419!2sar!4v1753453512711!5m2!1ses-419!2sar";
+const coordenadas = "-31.3589520, -64.2234330";
 const formAsistencia = "https://wa.me/5493513283382";
 const formCanciones = "https://forms.gle/D9trxxRy8nJUSyTZ8";
