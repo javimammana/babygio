f0d073f41d759119f19c4bf78f3a85fd37c46579
javimammana <javiermammana.dev@gmail.com>
2025-07-26 09:52:40 -0300

    correccion de hora

--- js/datos.js
+++ js/datos.js
@@ -1,6 +1,6 @@
 const fecha = new Date("08/10/2025 14:00");
 const cumple = new Date("08/10/2025");
-const horaEvento = "17:00 a 20:00Hs";
+const horaEvento = "16:00 a 19:00Hs";
 const direccionLink = "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d851.7502569884917!2d-64.22407673042716!3d-31.35895085319143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDIxJzMyLjIiUyA2NMKwMTMnMjQuNCJX!5e0!3m2!1ses-419!2sar!4v1753453512711!5m2!1ses-419!2sar";
 const coordenadas = "-31.3589520, -64.2234330";
 const formAsistencia = "https://wa.me/5493513283382";
